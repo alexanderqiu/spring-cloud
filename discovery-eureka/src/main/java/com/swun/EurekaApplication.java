c6b006bf5d82bdaa75b669860fe5ae6fabce3546
裘江龙 <qiujianglong@u51.com>
2017-06-02 13:52:03 +0800

    spring cloud学习

---
+++ discovery-eureka/src/main/java/com/swun/EurekaApplication.java
@@ -0,0 +1,19 @@
+package com.swun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by qiujianglong on 2017/6/2
+ * 使用Eureka做服务发现
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+
+}
